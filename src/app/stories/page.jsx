61da985a04671526afa9d0da31418dd62d5bcbe9
Victor Fawole <victorfawole@gmail.com>
2025-09-03 22:40:49 -0400

    feat: fresh start

--- src/app/stories/page.jsx
+++
@@ -1,9 +0,0 @@
-const Stories = () => {
-    return (
-        <div>
-            <h1>The section where I yap. If you found this, good job.</h1>
-        </div>
-    );
-}; 
-
-export default Stories;
